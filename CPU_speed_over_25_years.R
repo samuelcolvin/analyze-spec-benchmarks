db14c6b3d36ec082fece8c3e7c024746732e51b6
Samuel Colvin <s@muelcolvin.com>
2019-07-04 17:42:14 +0100

    tweak for prettier image

--- CPU_speed_over_25_years.R
+++ CPU_speed_over_25_years.R
@@ -3,7 +3,7 @@ library(rjson)
 library(jsonlite)
 library(lubridate)
 
-json_file <- "/Users/laurence/Projects/analyze-spec-benchmarks/charts/scatter-data.json"
+json_file <- "./charts/scatter-data.json"
 json <- fromJSON(json_file, flatten=TRUE)
 
 # Subset for ease of plotting
@@ -19,18 +19,20 @@ p <- json %>%
   geom_smooth(formula=y ~ poly(x, 2, raw=TRUE)) + 
   labs(title='CPU Speed Over 25 Years', x='Date', y='Speed (GHz)') +
   theme_minimal() +
-  theme(plot.title = element_text(size=24), 
-        axis.text.x = element_text(size=16),
-        axis.text.y = element_text(size=16),
-        axis.title = element_text(size=18),
+  theme(plot.title = element_text(size=24, color = "white"),
+        axis.text.x = element_text(size=16, color = "white"),
+        axis.text.y = element_text(size=16, color = "white"),
+        axis.title = element_text(size=18, color = "white"),
         rect = element_rect(fill = "transparent"), # all rectangles
-        panel.background = element_rect(fill = "transparent"), # bg of the panel
+        panel.background = element_rect(fill = "transparent", color = "white"), # bg of the panel
         plot.background = element_rect(fill = "transparent", color = NA), # bg of the plot
+        # panel.background = element_blank(), # get rid of border
+        panel.border = element_blank(), # get rid of border
         panel.grid.major = element_blank(), # get rid of major grid
         panel.grid.minor = element_blank(), # get rid of minor grid
-        legend.background = element_rect(fill = "transparent"), # get rid of legend bg
-        legend.box.background = element_rect(fill = "transparent") # get rid of legend panel bg
+        legend.background = element_rect(fill = "transparent", color = "white"), # get rid of legend bg
+        legend.box.background = element_rect(fill = "transparent", color = "white") # get rid of legend panel bg
         ) + 
   scale_y_continuous(limits=c(0, 5))
 
-ggsave(p, filename = "cpu_speed.png",  bg = "transparent")
+ggsave(p, filename = "cpu_speed.png",  bg = "transparent", width = 12, height = 7)
